5c0fdff4978466bbc59703a6dbc9b6e594f8adf7
Chiranth Bhaskar <chiranthjeev@Chiranths-MacBook-Air.local>
2025-06-28 00:15:14 +0530

    Added setup.py and made repo pip-installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='biopython',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    description='Custom biopython package',
+    url='https://github.com/ckb29/biopython',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
